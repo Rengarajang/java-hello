32abc57762326358fe25dec6969cb7f2606bd992
rengarajang <rengs14@gmail.com>
2019-04-15 15:20:09 +0000

    sample project

---
+++ my-app/src/main/java/com/rengscloud/app/App.java
@@ -0,0 +1,13 @@
+package com.rengscloud.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! JAVA!!" );
+    }
+}
